ed882354f7357459c24511a62a34e81547111c57
Juan Pablo Gomez Pichardo <juanpablo92002@gmail.com>
2024-09-26 23:20:28 -0600

    Agregue informacion

---
+++ JuanPabloGomezPichardo/myAppJ/App.js
@@ -0,0 +1,61 @@
+import { StatusBar } from 'expo-status-bar';
+import {Pressable, StyleSheet, View, Image, Text } from 'react-native';
+
+
+const PlaceholderImage = require('./assets/images/background-image.png');
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+      <Image source={PlaceholderImage} style={styles.image}/>
+      <Text style={styles.infoText}> Bienvenidos a la aplicacion web</Text>
+      </View>
+      <StatusBar style="auto" />
+      <Pressable style={styles.button}>
+      <Text style={styles.text}>Presiona aqui</Text>
+    </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 20,
+    paddingHorizontal: 80,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: 'blue',
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
+  },
+  infoText:{
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    textAlign: 'center',
+    color: 'white',
+  },
+});
