31bab230a2708cc542f802351205859073cb28c4
Erick Pineda <erifabrizz07@gmail.com>
2024-10-04 15:31:15 -0600

    colors y fonts

--- ERICK_FABRIZZIO_PINEDA_HERRERA/myApp/App.js
+++ ERICK_FABRIZZIO_PINEDA_HERRERA/myApp/App.js
@@ -1,19 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, Image, Button } from 'react-native';
+import { StyleSheet, View, Image, Button, Text, TextInput, TouchableOpacity, ImageBackground } from 'react-native';
 
 const PlaceholderImage = require('./assets/background.jpg');
 
 export default function App() {
   const handlePress = () => {
-    alert('Botón presionado');
+    alert('Cuenta no encontrada');
   };
   return (
     <View style={styles.container}>
-      <View style={styles.imageContainer}>
-        <Image source={PlaceholderImage} style={styles.image} />
-      </View>
-      <Button title="Presionar" onPress={handlePress} />
-      <StatusBar style="auto" />
+      <ImageBackground source={PlaceholderImage} resizeMode="cover" style={styles.image}>
+        <Text style={styles.title}>Bienvenido</Text>
+        <Text style={styles.subtitle}>Registrarme</Text>
+        <TextInput placeholder='Nombre(s)' style={styles.textInput}></TextInput>
+        <TextInput placeholder='Apellidos' style={styles.textInput}></TextInput>
+        <TextInput placeholder='Telefono' style={styles.textInput}></TextInput>
+        <StatusBar style="auto" />
+        <TouchableOpacity onPress={(handlePress)}>
+          <Text style={{padding:10, color:"#000", fontSize: 17, backgroundColor: "#c5cac4", marginTop:20, width: "30%"}}>Registrarme</Text>
+        </TouchableOpacity>
+      </ImageBackground>
     </View>
   );
 }
@@ -21,18 +27,38 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#25292e',
+    backgroundColor: '#EBE1C9',
     alignItems: 'center',
     justifyContent: 'center',
   },
-  imageContainer: {
-    flex: 1,
-    paddingTop: 58,
-    justifyContent: 'center',
-  },
   image: {
-    width: 320,
-    height: 440,
-    borderRadius: 18,
+    width: "100%",
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize : 50,
+    color: '#000',
+    fontWeight: "bold",
+    alignContent: "center"
+  },  
+  subtitle:{
+    fontSize : 30,
+    color: '#000',
+    fontWeight: "bold",
+    alignContent: "center"
+  },
+  textInput:{
+    fontSize: 20,
+    borderWidth: 2,
+    borderColor: "gray",
+    backgroundColor: "#F5FEFF",
+    padding: 10,
+    width: "80%",
+    height: 50,
+    marginTop: 10,
+    marginBottom: 20,
+    borderRadius: 20,
   },
 });
---
+++ ERICK_FABRIZZIO_PINEDA_HERRERA/myApp/constants/Colors.js
@@ -0,0 +1,9 @@
+export default{
+    cinnabar: '#f24236',
+    darkPink: '#F279B2',
+    light: '#F2D8E4',
+    HotPink: '#F26DB6',
+    lightPink: '#F2BBD5',
+    MediumPink: '#F299C2',
+    
+};
