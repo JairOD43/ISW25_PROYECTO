6164f8b1e304a70b1c78241443909b366f79a1e4
panke <panke.gmr@gmail.com>
2024-10-04 15:49:09 -0600

    cambio fuentes

--- Guadalupe_Rangel/myApp/App.js
+++ Guadalupe_Rangel/myApp/App.js
@@ -1,22 +1,26 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Text, TextInput,ImageBackground} from "react-native";
 
 import Button from './components/Button'; 
 import ImageViewer from './components/ImageViewer'; 
 
-const PlaceholderImage = require("./assets/prueba.jpg"); // Asegúrate de que este sea el nombre correcto de tu imagen.
+const image = require("./assets/prueba.jpg"); // Asegúrate de que este sea el nombre correcto de tu imagen.
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.imageContainer}>
-        <ImageViewer placeholderImageSource={PlaceholderImage} />
-      </View>
-      <View style={styles.footerContainer}>
-        <Button label="Choose a photo" />
-        <Button label="Use this photo" />
-      </View>
-      <StatusBar style="auto" />
+       <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+        <Text style= {styles.title}>WELCOME</Text>
+        <Text style= {styles.subtitle}>Sing in to your account</Text>
+        <TextInput placeholder="aaa@xxx.com"/>
+        <TextInput placeholder="password"/>
+        <TextInput placeholder="numero telefonico"/>
+        <View style={styles.footerContainer}>
+          <Button label="Registrarse" />
+      
+        </View>
+        <StatusBar style="auto" />
+      </ImageBackground>
     </View>
   );
 }
@@ -27,13 +31,36 @@ const styles = StyleSheet.create({
     backgroundColor: '#25292e',
     alignItems: 'center',
   },
-  imageContainer: {
+  image:{
     flex: 1,
-    paddingTop: 58,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: "100%"
+
   },
   footerContainer: {
     flex: 1 / 3,
     alignItems: 'center',
   },
+  title:{
+    paddingTop:50,
+    fontSize:50,
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  subtitle:{
+    fontSize:20,
+    color: 'red',
+    fontWeight: 'condensedBold',
+  },
+  TextInput:{
+    borderWidth:10,
+    borderColor:'#fff',
+    padding: 10,
+    width:"80%",
+    height:50,
+    marginTop:20,
+    borderRadius:30,
+    backgroundColor:'#f0f0f0',
+  },
 });
-
