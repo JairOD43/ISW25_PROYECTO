28e1914f9f9009c2ba81ab16d1733b46a45d4dd4
Oscar-oky <oscargalvan2724@gmail.com>
2024-10-04 15:36:21 -0600

    Agregue carpetas de components y controls

--- Oscar_Alexandro_Morales_Galvan/myApp/App.js
+++ Oscar_Alexandro_Morales_Galvan/myApp/App.js
@@ -1,23 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ImageBackground, Button } from 'react-native';
-import React from 'react';
+import { Button, StyleSheet, Text, TextInput, TouchableOpacity, View, ImageBackground } from 'react-native';
+
+const PlaceholderImage = require('./assets/images/background-image.png');
 
 export default function App() {
   return (
-    <ImageBackground
-      source={{ uri: 'https://docs.expo.dev/static/images/tutorial/background-image.png' }}
-      style={styles.background}
-      resizeMode="cover"
-    >
+    <ImageBackground source={PlaceholderImage} style={styles.background}>
       <View style={styles.container}>
-        <Text style={{ color: '#fff', textAlign: 'center', marginBottom: 20 }}>
-          Open up App.js welcome to my App!
-        </Text>
-        <Button
-          title="Boton"
-          onPress={() => alert('Button pressed!')}
-        />
+        <Text style={styles.title}>Welcome</Text>
+        <Text style={styles.subtitle}>Sign in to your account</Text>
+
+        <TextInput style={styles.textInput} placeholder="Nombres" />
+        <TextInput style={styles.textInput} placeholder="Apellidos" />
+        <TextInput style={styles.textInput} placeholder="aaa@xxx.com" />
+        <TextInput style={styles.textInput} placeholder="password" />
+        <Button onPress={() => console.log('Mi mensaje uno')} title="boton 1" />
+
         <StatusBar style="auto" />
+
+        <TouchableOpacity onPress={() => console.log('Mi mensaje dos')} title="boton 1">
+          <Text style={styles.buttonText}>Boton 2</Text>
+        </TouchableOpacity>
       </View>
     </ImageBackground>
   );
@@ -25,13 +28,42 @@ export default function App() {
 
 const styles = StyleSheet.create({
   background: {
-    flex: 1, 
-    justifyContent: 'center',
+    flex: 1,
+    justifyContent: 'center', 
     alignItems: 'center', 
   },
   container: {
-    backgroundColor: 'rgba(37, 41, 46, 0.7)', 
+    flex: 1,
+    backgroundColor: 'rgba(255, 255, 255, 0.8)', 
+    alignItems: 'center',
+    justifyContent: 'center',
     padding: 20,
     borderRadius: 10, 
+  },
+  title: {
+    fontSize: 80,
+    color: "#000",
+    fontWeight: "bold",
+  },
+  subtitle: {
+    fontSize: 20,
+    color: "gray",
+    fontWeight: "600", // Peso de fuente fijo
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "gray",
+    padding: 10,
+    width: "80%",
+    height: 50,
+    marginTop: 20,
+    borderRadius: 30,
+    backgroundColor: "#fff", // Fondo blanco
+  },
+  buttonText: {
+    padding: 10,
+    color: 'white',
+    fontSize: 30,
+    backgroundColor: "gray",
   },
 });
