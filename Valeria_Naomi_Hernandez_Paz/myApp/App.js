ea062b641d3653aa83c3fef214a79e47c2eed4be
Val <geherdiell.asr@gmail.com>
2024-10-04 12:01:54 -0600

    colors y fonts

--- Valeria_Naomi_Hernandez_Paz/myApp/App.js
+++ Valeria_Naomi_Hernandez_Paz/myApp/App.js
@@ -1,38 +1,60 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View} from "react-native";
+import { ImageBackground, StyleSheet, Text, TextInput,TouchableOpacity,  View } from 'react-native';
 
-import Button from './components/Button'; 
-import ImageViewer from './components/ImageViewer';
-
-const PlaceholderImage = require("./assets/images/background-image.png");
+const image = { uri: "https://docs.expo.dev/static/images/tutorial/background-image.png" };
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View style={styles.imageContainer}>
-        <ImageViewer placeholderImageSource={PlaceholderImage} />
-      </View>
-      <View style={styles.footerContainer}>
-        <Button label="Choose a photo" />
-        <Button label="Use this photo" />
-      </View>
-      <StatusBar style="auto" />
-    </View>
+  <View style={styles.container}>
+    <ImageBackground source={image} style={styles.image}>
+     
+      <TextInput style={styles.TextInput}placeholder='Nombres'/>
+      <TextInput style={styles.TextInput}placeholder='Telefono'/>
+    
+         <TouchableOpacity onPress={()=>console.log("Ha sido registrado")}>
+          <Text style={{padding:10, color:'white', fontSize:30,borderWidth: 1, marginTop: 40,borderRadius: 20, width: '50%', }}>Registrar</Text>
+         </TouchableOpacity>
+    </ImageBackground>
+  </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#25292e',
-    alignItems: 'center',
+    flexDirection: 'column',
   },
-  imageContainer: {
+  image: {
     flex: 1,
-    paddingTop: 58,
+    resizeMode: 'cover',
+    justifyContent: 'center',
   },
-  footerContainer: {
-    flex: 1 / 3,
-    alignItems: 'center',
+  text: {
+    color: 'white',
+    fontSize: 42,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    backgroundColor: '#000000a0',
   },
+  TextInput:{
+    borderWidth: 1.5,
+    paddingStart: 30,
+    borderColor:'white',
+    padding: 10,
+    width: '80%',
+    color: 'white',
+    height: 40,
+    marginTop: 20,
+    borderRadius: 30,
+     },
+     Button: {
+      borderWidth: 1,
+      paddingStart: 30,
+      borderColor:'gray',
+      padding: 10,
+      width: '50%',
+      height: 50,
+      marginTop: 20,
+      borderRadius: 30,
+     },
+  
 });
