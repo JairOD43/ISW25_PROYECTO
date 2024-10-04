dde5de07cea4daa7fdd5f87c397d98c05ac02cc5
TheUndertaker2004 <022000461@upsrj.edu.mx>
2024-10-04 09:19:05 -0600

    Cree Nuevos archivos JavaScript

--- Jesus_Santiago_Serrano_Ruiz/myApp/App.js
+++ Jesus_Santiago_Serrano_Ruiz/myApp/App.js
@@ -1,19 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, ImageBackground } from 'react-native';
+import { StyleSheet, Text, View, Button, ImageBackground, TextInput, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
-    <ImageBackground 
-      source={{uri: 'https://i.pinimg.com/736x/8d/62/1f/8d621fdcbda3cac762100b026f3e7db7.jpg'}} 
+    <ImageBackground
+      source={{ uri: 'https://wallpapers.com/images/hd/the-rock-in-gym-training-wctegg23m9yj1rb4.jpg' }}
       style={styles.backgroundImage}
     >
       <View style={styles.container}>
-        <Text style={styles.text}>Welcome to My App!</Text>
-        <Button 
-          title="Press Me"
-          onPress={() => alert('Button Pressed!')}
-        />
+        <Text style={styles.title}>WELCOME TO</Text>
+        <Text style={styles.title}>DORIAN HOUSE</Text>
+        <Text style={styles.text}>Sign in to your account</Text>
+        <TextInput placeholder='Name/LastName' style={styles.textInput} />
+        <TextInput placeholder='PassWord' style={styles.textInput} />
+        <TextInput placeholder='PhoneNumber' style={styles.textInput} secureTextEntry={true} />
         <StatusBar style="auto" />
+
+        <TouchableOpacity onPress={() => console.log("Me presionaste GG")}>
+          <Text
+            style={{
+              padding: 10,
+              color: "white",
+              fontSize: 25,
+              backgroundColor: "green",
+              textAlign: "center",
+              marginTop: 20,
+              borderRadius: 30,
+            }}
+          >
+            Create User
+          </Text>
+        </TouchableOpacity>
       </View>
     </ImageBackground>
   );
@@ -31,8 +48,24 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   text: {
-    color: '#fff',
+    color: 'white',
     fontSize: 24,
     marginBottom: 20,
+  },
+  title: {
+    color: 'orange',
+    fontSize: 60,
+    marginBottom: 20,
+    fontWeight: "bold",
+  },
+  textInput: {
+    backgroundColor:"white",
+    borderWidth: 1,
+    borderColor: "green",
+    padding: 10,
+    width: "80%",
+    height: 50,
+    marginTop: 20,
+    borderRadius: 30,
   },
 });
